c9f5352f396f682cb1665bd0f83e71d607d42aa4
zhang lihui <1962840289@qq.com>
2022-02-25 20:22:25 -0500

    add assignment list page

--- client/src/Public/Course/CourseHomePage.tsx
+++ client/src/Public/Course/CourseHomePage.tsx
@@ -1,17 +1,34 @@
 import React, {useState, useRef} from "react";
-import { Box, Button } from "@mui/material";
+import { useParams } from 'react-router-dom';
+import { Box, Button, Grid, Divider } from "@mui/material";
 import { Descendant } from "slate";
 import { Editor } from "./Editor";
 import axios from "axios";
 import config from "../../config";
 import EditIcon from '@mui/icons-material/Edit';
+import useTodoList from "../../Hooks/todoList";
+import { TodoItem } from "../../Hooks/interfaces";
 
-type courseHomePageProps = { homepage: Descendant[], role: string, code: string };
+type courseHomePageProps = { homepage: Descendant[], role: string, code: string, userName: string };
+
+function oneTodo(todoItem: TodoItem, key: number) {
+    const {name, point, dueDate} = todoItem;
+
+    return (
+        <Box className="todo-item" key={key}>
+            <p className="todo-name">{name}</p>
+            <p>Point: {point} |</p>
+            <p>Due: {dueDate}</p>
+        </Box>
+    )
+}
 
 function CourseHomePage(props: courseHomePageProps) {
+    const { courseCode } = useParams();
     const [courseHomePage, setCourseHomePage] = useState<Descendant[]>(props.homepage);
     const [editing, setEditing] = useState<boolean>(false);
     const editorRef = useRef({value: []});
+    const todoList = useTodoList(props.userName, parseInt(courseCode ? courseCode : "0"));
 
     const handleEditBtnCLick = () => {
         if (editing) {
@@ -30,12 +47,24 @@ function CourseHomePage(props: courseHomePageProps) {
 
     return (
         <Box>
-            <Editor readOnly={!editing} initialvalue={courseHomePage} parentRef={editorRef} />
-            {props.role === "student" ? 
-                <Button variant="contained" endIcon={<EditIcon />} size="large" sx={{position: "fixed", bottom: 100, right: "40%"}} onClick={handleEditBtnCLick}>
-                    {editing? "Save" : "Edit"}
-                </Button> 
-                : null}
+            <Grid container spacing={4} columns={{xs: 4, sm: 8, md: 12, lg: 12, xl: 16}}>
+                <Grid item xs={3} sm={5} md={9} lg={8} xl={10}>
+                    <Editor readOnly={!editing} initialvalue={courseHomePage} parentRef={editorRef} />
+                    {props.role !== "student" ? 
+                        <Button variant="contained" endIcon={<EditIcon />} size="large" sx={{position: "fixed", bottom: 100, right: "40%"}} onClick={handleEditBtnCLick}>
+                            {editing? "Save" : "Edit"}
+                        </Button> 
+                        : null}
+                </Grid>
+
+                <Grid item xs={1} sm={3} md={3} lg={4} xl={6}>
+                    Todo
+                    <Divider className="home-divider"/>
+                    {todoList.length > 0 ? todoList.map((item, key) => oneTodo(item, key)) : <Box sx={{ fontWeight: "bold", margin: "20px" }}>Well Done~</Box>}
+                    Recent Feedback
+                    <Divider className="home-divider"/>
+                </Grid>
+            </Grid>
         </Box>
     )
 }
--- client/src/Public/Course/index.tsx
+++ client/src/Public/Course/index.tsx
@@ -4,25 +4,12 @@ import axios from 'axios';
 import config from "../../config";
 import { Box, Grid, List, ListItem, ListItemText, Divider } from "@mui/material";
 import { Descendant } from "slate";
-import useTodoList from "../../Hooks/todoList";
 import { Routes, Route, Link } from "react-router-dom";
-import { TodoItem } from "../../Hooks/interfaces";
 import CourseHomePage from "./CourseHomePage";
+import AssignmentList from "./AssignmentList";
 
 type courseProps = {userName: string, role: string};
 type Tab = {name: string, visible: boolean};
-
-function oneTodo(todoItem: TodoItem, key: number) {
-    const {name, point, dueDate} = todoItem;
-
-    return (
-        <Box className="todo-item" key={key}>
-            <p className="todo-name">{name}</p>
-            <p>Point: {point} |</p>
-            <p>Due: {dueDate}</p>
-        </Box>
-    )
-}
 
 function Course(props: courseProps) {
     const { courseCode } = useParams();
@@ -31,7 +18,6 @@ function Course(props: courseProps) {
     const [department, setDepartment] = useState<string>("");
     const [term, setTerm] = useState<string>(""); 
     const [tabs, setTabs] = useState<Tab[]>([]);
-    const todoList = useTodoList(props.userName, parseInt(courseCode ? courseCode : "0"));
 
     useEffect(() => {
         axios.get(`${config.baseUrl}/coursepage`, { params: {courseCode} })
@@ -50,7 +36,7 @@ function Course(props: courseProps) {
     useEffect(() => {
         axios.get(`${config.baseUrl}/term`)
         .then(res => {
-            const term  = res.data;
+            const term = res.data;
             setTerm(term);
         })
         .catch(error => {
@@ -77,24 +63,16 @@ function Course(props: courseProps) {
                     </List>
                 </Grid>
 
-                <Grid item xs={3} sm={5} md={9} lg={8} xl={10}>
+                <Grid item xs={4} sm={8} md={12} lg={12} xl={16}>
                     <Routes>
-                        <Route path="" element={loaded? <CourseHomePage homepage={courseHomePage} role={props.role} code={courseCode!}/> : <Box></Box>}></Route>
+                        <Route path="" element={loaded? <CourseHomePage homepage={courseHomePage} role={props.role} code={courseCode!} userName={props.userName}/> : <Box></Box>}></Route>
                         <Route path="announcement" element={<div>announcement</div>}></Route>
-                        <Route path="assignment" element={<div>assignment</div>}></Route>
+                        <Route path="assignment" element={<AssignmentList role={props.role} code={courseCode!}/>}></Route>
                         <Route path="grade" element={<div>grade</div>}></Route>
                         <Route path="disccusion" element={<div>disccusion</div>}></Route>
                         <Route path="files" element={<div>files</div>}></Route>
                         <Route path="people" element={<div>people</div>}></Route>
                     </Routes>
-                </Grid>
-
-                <Grid item xs={1} sm={3} md={3} lg={4} xl={6}>
-                    Todo
-                    <Divider className="home-divider"/>
-                    {todoList.length > 0 ? todoList.map((item, key) => oneTodo(item, key)) : <Box sx={{ fontWeight: "bold", margin: "20px" }}>Well Done~</Box>}
-                    Recent Feedback
-                    <Divider className="home-divider"/>
                 </Grid>
             </Grid>
         </Box>
